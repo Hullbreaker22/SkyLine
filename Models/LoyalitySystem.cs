1fc9fe2cc753b86e81902c88a99eb1de8df5f24b
AD <AD@DESKTOP-211M1RP>
2025-10-29 13:36:27 +0300

    firstChane

---
+++ Models/LoyalitySystem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkyLine.Models
+{
+    public class LoyalitySystem
+    {
+        [Key]
+        public int Loyality_Id_PK { get; set; }
+
+        [ForeignKey(nameof(User))]
+        public string User_Id_FK { get; set; } = string.Empty;
+
+        public int Points { get; set; }
+        public string TierLevel { get; set; } = "Basic";
+
+        public ApplicationUser? User { get; set; }
+    }
+}
